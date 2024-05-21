ddedfdaec36eae8915c373ecc5cbb5d3884da980
abhijit <kamathabhijit2002@gmail.com>
2024-05-21 13:13:05 -0400

    wrote a solution + an optimal solution to consider from neetcode

--- Medium/3/abhijit.py
+++ Medium/3/abhijit.py
@@ -1,25 +1,45 @@
-# need to polish solution. Not fully functioning
-import string
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_hash = dict.fromkeys(string.printable, None) # of the values we've seen
-        largest_substring = None
-        if(len(s) == 0):
-            return 0
-    
-    
-        for curr_index, char in enumerate(s):
-            if char_hash[char] == None:
-                char_hash[char] = curr_index + 1
-            else:
-                if largest_substring == None:
-                    largest_substring = curr_index + 1 - char_hash[char]
-                else:
-                    largest_substring = max(largest_substring, curr_index - char_hash[char])
-                    char_hash[char] = curr_index + 1
-    
-        if largest_substring == None:
-            return len(s)
-        else:
-            return largest_substring
+        # explanation
+        # abca 
+        # // first case. where the incoming character s at the start of the seen string
+        # // we truncate remoiving index 0 and continue to move forward
+        # pwwkew
+        # pww
+        # // second case. were the incoming character is at the end of the seen string. 
+        # // we discard everything that comes before and start fresh as there will be a gap
+        # // we are looking substrings and not subsequences
         
+        # we could just slice the string from where we last saw the element and then continue from there
+
+        seen = []
+        current = []
+        if len(s) == 0:
+            #simple base case to handle empty string 
+            return 0
+        
+        for i in s:
+            print(i)
+            print(current)
+            if i in current:
+                if(i == current[0]):
+                    seen.append("".join(current))
+                    current = current[1:]
+                    current.append(i)
+                else:
+                    index = current.index(i)
+                    seen.append("".join(current))
+                    current = current[index + 1:]
+                    current.append(i)
+            else:
+                current.append(i)
+        
+        # handles the final string that we end on
+        seen.append("".join(current))
+
+        print(seen)
+        lengths = [len(s) for s in seen]
+        print("lengths",lengths)
+        print(max(lengths))
+        return max(lengths)
+     

697cae290f6ec8d757da6e5c112058ffe78c49bf
abhijit <kamathabhijit2002@gmail.com>
2024-08-12 19:18:05 -0400

    typo changes

--- Easy/703/abhijit.py
+++ Easy/703/abhijit.py
@@ -2,14 +2,15 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         # create a min heap of size k
         self.heap = nums
+        self.k = k
         heapq.heapify(self.heap)
-        while len(self.heap) > k:
+        while len(self.heap) > self.k:
             heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
         heapq.heappush(self.heap,val)
         
-        while len(self.heap) > k:
+        while len(self.heap) > self.k:
             heapq.heappop(self.heap)
         
         return self.heap[0]

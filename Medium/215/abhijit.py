86ec8c04859b96b4e8f6dd793bface1d644d7889
abhijit <kamathabhijit2002@gmail.com>
2024-08-12 20:18:32 -0400

    heap trick

---
+++ Medium/215/abhijit.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for num in nums[k:]:
+            if num > heap[0]:
+                heapq.heappush(heap,num)
+                heapq.heappop(heap)
+        
+        return heap[0]

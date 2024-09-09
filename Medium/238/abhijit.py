120c1299e6f1e6bd90806fbb4b819b947e06df9b
abhijit <kamathabhijit2002@gmail.com>
2024-09-09 10:33:19 -0400

    fixing up 238

--- Medium/238/abhijit.py
+++ Medium/238/abhijit.py
@@ -1,29 +1,42 @@
 class Solution:
     
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        leftProdArray = []
-        rightProdArray = [0] * len(nums)
-        returnArray = []
-        for i in range(len(nums)):
-            if(i > 0):
-                leftProdArray.append(nums[i] * leftProdArray[i-1])
-            else:
-                leftProdArray.append(nums[i])
         
-        print(leftProdArray)
-        for i in range(len(nums) -1, -1,-1):
-            if(i < len(nums) -1):
-                rightProdArray[i] = nums[i] * rightProdArray[i + 1]
-            else:
-                rightProdArray[i] = nums[i]
+        prefix_product = nums[0]
+        prefix_arr = [nums[0]]
+
+        suffix_product = nums[-1]
+        suffix_arr = [nums[-1]]
+        
+
+        for i in range(1,len(nums)):
+            prefix_arr.append(prefix_product)
+            prefix_product *= nums[i]
             
-        print(rightProdArray)
+           
+        
+        for i in range(len(nums) - 2,-1,-1):
+            suffix_arr.append(suffix_product)
+            suffix_product *= nums[i]
+            
+        print(prefix_arr)
+        print(suffix_arr)
+
+        result = []
+
         for i in range(len(nums)):
             if i == 0:
-                returnArray.append(rightProdArray[i + 1])
-            elif i == len(nums) -1 :
-                returnArray.append(leftProdArray[i-1])
+                result.append(suffix_arr[ -1 * (i + 1)])
+            elif i != len(nums) -1 :
+                pre = prefix_arr[i]
+                suffix = suffix_arr[ -1 * (i + 1)]
+                print("pre", pre, "suffix", suffix)
+                result.append(pre*suffix)
             else:
-                returnArray.append(rightProdArray[i+1] * leftProdArray[i-1])
-        return returnArray
+                result.append(prefix_arr[i])
 
+        return result
+
+
+
+        

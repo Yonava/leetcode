33fcaf58c09d681747dda293c29947027ba48472
abhijit <kamathabhijit2002@gmail.com>
2024-09-22 09:29:06 -0400

    solved this myself

--- Medium/22/abhijit.py
+++ Medium/22/abhijit.py
@@ -1,37 +1,26 @@
-import itertools
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        all_possible = ["".join(seq) for seq in itertools.product("()", repeat=n)]
-        print(all_possible)
-        parens = set()
-        for permutation in all_possible:
-            str_pointer = 0
-            return_str = ""
-            currStack = []
-            while str_pointer < len(permutation):
-                if len(currStack) == 0:
-                    currStack.append(permutation[str_pointer])
-                    str_pointer += 1
-                else:
-                    if permutation[str_pointer] != currStack[-1]:
-                        temp_str = ""
-                        while len(currStack) != 0:
-                            temp_str = "(" + temp_str + ")"
-                            currStack.pop()
-                        return_str = return_str + temp_str
-                    else:
-                        currStack.append(permutation[str_pointer])
-                        str_pointer += 1
-
-            if len(currStack) != 0:
-                temp_str = ""
-                while len(currStack) != 0:
-                    temp_str = "(" + temp_str + ")"
-                    currStack.pop()
-                return_str = return_str + temp_str
-
-            print(f"working with {permutation} and obtained {return_str}")
-            parens.add(return_str)
-        return parens
+        returnSet = set()
         
-''' comeback after exploring backtracking a little more'''
+        def close(string):
+            pad = 0
+            for i in range(len(string)-1,-1,-1):
+                if string[i] == "(":
+                    pad += 1
+                else:
+                    pad -= 1
+    
+            return string + pad * ")" 
+        
+        def search(curr_str,opening_paren_count,closing_paren_count):
+            
+            if opening_paren_count == n :
+                returnSet.add(close(curr_str))
+                return
+            search(curr_str + "(",opening_paren_count+1,closing_paren_count)
+            if len(curr_str) and opening_paren_count > closing_paren_count:
+                search(curr_str + ")",opening_paren_count,closing_paren_count + 1) #
+
+        search("",0,0)
+        # print("testing",close("()()("))
+        return returnSet

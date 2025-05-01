1e334865642ac926dcf714799ef6eb14db2a3839
abhijit <kamathabhijit2002@gmail.com>
2025-04-30 20:59:45 -0400

    leetcode 295 hard

---
+++ Hard/295/abhijit.py
@@ -0,0 +1,39 @@
+class MedianFinder:
+
+    def __init__(self):
+        # small is max heap
+        # large is mean heap
+        self.small, self.large = [], []
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+
+        # make sure every num in small is <= every num in large
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        # if size is not a differnece of 1
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+            
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            #  odd number of elements
+            return - 1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        return ( -1 * self.small[0] + self.large[0]) / 2
+
+
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder() 
+# obj.addNum(num)
+# param_2 = obj.findMedian()

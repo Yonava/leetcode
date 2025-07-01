7321dc2824ab5801e0987bf8dd9f1fd2b89854c7
abhijit <kamathabhijit2002@gmail.com>
2025-07-01 12:07:17 -0700

    my way of doing the same problem

--- Medium/82/abhijit.py
+++ Medium/82/abhijit.py
@@ -1,23 +1,26 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(-1, head)
-        prev = dummy
-        current = head
+        currPointer = ListNode(-1, head)  # Just as a helper for linking; not used for return
+        currPointer.next = head
+        currentNode = head
 
-        while current:
-            # Check if current is start of a duplicate sequence
-            has_duplicate = False
-            while current.next and current.val == current.next.val:
-                current = current.next
-                has_duplicate = True
-            
-            if has_duplicate:
-                # Skip all duplicates
-                prev.next = current.next
-            else:
-                # No duplicate, move prev
-                prev = prev.next
-            
-            current = current.next
-        
-        return dummy.next
+        if head is None:
+            return head
+
+        while currentNode and currentNode.next:
+            if currentNode.val != currentNode.next.val:
+                if currPointer.next == currentNode:
+                    currPointer = currentNode
+                else:
+                    if currPointer.next == head:
+                        head = currentNode.next
+                    currPointer.next = currentNode.next
+            currentNode = currentNode.next
+
+        # Handle the last group
+        if currPointer.next != currentNode:
+            if currPointer.next == head:
+                head = currentNode.next
+            currPointer.next = currentNode.next
+
+        return head

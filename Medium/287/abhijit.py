4ffd545aa59556f52f5c9053c535324fe894b593
abhijit <kamathabhijit2002@gmail.com>
2025-07-03 13:16:44 -0700

    cyclic sort solution variation

--- Medium/287/abhijit.py
+++ Medium/287/abhijit.py
@@ -11,4 +11,19 @@ class Solution:
         
         return -1
     
-    
+# cyclic sort solution
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # TODO: Write your code here
+        i = 0
+        while i < len(nums):
+            val = nums[i]
+            if i == (val - 1):
+                i += 1
+                continue
+            elif nums[val - 1] == val:
+                # before swapping the condition is satisfied
+                return val
+            else:
+                nums[i] = nums[val -1]
+                nums[val - 1] = val

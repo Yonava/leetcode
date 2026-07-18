11b91648fd515967f5b0ea8412d86d5d649426b4
abhijit <kamathabhijit2002@gmail.com>
2026-07-18 12:56:15 -0700

    o(n) fixing up with previous pointers

--- Medium/143/abhijit.py
+++ Medium/143/abhijit.py
@@ -8,27 +8,43 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        second = slow.next
-        slow.next = None # split the list
 
-        #reverse list
-        prev = None
-        while second:
-            temp = second.next
-            second.next = prev
-            prev = second
-            second = temp
+        def display(h):
+            arr = []
+            while h is not None:
+                arr.append(h.val)
+                h = h.next
+            print(arr)
+
+
+        if head is None or head.next is None:
+            return head
+        start = head
+        curr = head
+        head.prev = None
+
         
-        # merge
-        first, second = head,prev
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            second = tmp2
-            first = tmp1
+        # print("here 0")
+        while curr.next is not None:
+            curr.next.prev = curr
+            curr = curr.next
+
+        last = curr
+        first = head
+        # print("here 1")
+        while first.next != last and first != last:
+            first_n = first.next
+            first.next = last
+            last.next = first_n
+            last = last.prev
+            last.next = None
+            first = first.next.next
+            # display(head)
+            # print(first.val, last.val)
+        
+        # print("here 2")
+        # while start.next is not None:
+        #     start.prev = None
+        
+        return start
+
